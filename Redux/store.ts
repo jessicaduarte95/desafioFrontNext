1a679526907a64d390ed50dc39865da672328d1a
jessicaduarte95 <jessicaduarte.95@gmail.com>
2024-03-18 12:07:09 -0300

    ajuste

--- Redux/store.ts
+++ Redux/store.ts
@@ -1,18 +1,35 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { configureStore, combineReducers } from '@reduxjs/toolkit'
 import { TypedUseSelectorHook, useSelector } from 'react-redux';
 import cadastroReducer from './Cadastro/slice';
 import stepsCadastro from './Cadastro/sliceSteps';
 import profissional from './Cadastro/sliceProfissional';
+import storage from 'redux-persist/lib/storage';
+import { persistReducer, persistStore } from 'redux-persist';
 
-export const store = configureStore({
-  reducer: {
-    cadastroReducer,
-    stepsCadastro,
-    profissional
-  },
+
+const persistConfig = {
+  key: 'root',
+  storage,
+};
+
+const rootReducer = combineReducers({
+  cadastroReducer,
+  stepsCadastro,
+  profissional,
 });
+
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+const store = configureStore({
+  reducer: persistedReducer,
+});
+
+const persistor = persistStore(store);
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
 
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
+
+export { store, persistor}
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,15 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app';
 import { Provider } from 'react-redux';
-import { store } from '../Redux/store';
+import { store, persistor } from '../Redux/store';
+import { PersistGate } from 'redux-persist/integration/react';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <Provider store={store}>
-      <Component {...pageProps} />
+      <PersistGate loading={null} persistor={persistor}>
+        <Component {...pageProps} />
+      </PersistGate>
     </Provider>
   )
 }
